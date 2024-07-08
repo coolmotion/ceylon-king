7915bcf950380fa7cc4d3e77e4dac77e89bdbecc
coolmotion <hcnpeiris@gmail.com>
2024-07-08 07:53:37 +0530

    update

--- web/views.py
+++ web/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from resturant.models import Category
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def index(request):
+    categories = Category.objects.all()
+    return render(request, 'index.html', {'categories': categories})
 
 
 def about(request):
